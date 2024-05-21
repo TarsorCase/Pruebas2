e8192949dc212cd428cba677a2e4b8016c9cd89c
TarsorCase <gerardgonzalez41@gmail.com>
2024-05-21 16:09:20 -0400

    Subiendo documentos

---
+++ Calculadora/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestDemo;
+
+Console.WriteLine("Calculadora");
+Console.Write("Ingrese un valor a sumar: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el segundo valor a sumar: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Mathematics.Addition(a, b));
